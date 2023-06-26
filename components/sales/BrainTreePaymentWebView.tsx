9ac41602f3de3b06f6445b793fe558dfdd2f6317
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-26 11:08:15 +0100

    .

--- components/sales/BrainTreePaymentWebView.tsx
+++ components/sales/BrainTreePaymentWebView.tsx
@@ -5,9 +5,29 @@ import axios from 'axios';
 
 const HOST = "https://candii4-backend2-3f9abaacb350.herokuapp.com";
 
-const BrainTreePaymentWebView = ({
-  onNonceRetrieved
-}) => {
+const BrainTreePaymentWebView = () => {
+
+    const onNonceRetrieved = async (nonce) => {
+        console.log('Nonce retrieved:', nonce); // Log the nonce value
+
+        try {
+            const response = await axios.post(`${HOST}/createPaymentTransaction`, {
+              amount: 0.01, 
+              nonce: nonce,
+            }, {
+              headers: {
+                'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate'
+              }
+            });
+
+            console.log('Server response:', response.data); // Log the server response
+
+            const { isPaymentSuccessful, errorText } = await response.data;
+            Alert.alert(isPaymentSuccessful ? "Payment successful" : `Payment error - ${errorText}`);
+        } catch (error) {
+            console.error('Error during payment:', error); // Log any error that occurs during the payment process
+        }
+      }
 
   return (
     <View style={{ height: 450 }}>
